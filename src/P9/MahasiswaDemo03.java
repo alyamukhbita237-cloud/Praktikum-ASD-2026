b41a3206a924167d38168c95a3cf2a9b637dc4c1
alyamukhbita237 <alyamukhbita237@gmail.com>
2026-04-24 09:04:40 +0700

    P9_Praktikum 1

---
+++ src/P9/MahasiswaDemo03.java
@@ -0,0 +1,59 @@
+package P9;
+
+import java.util.Scanner;
+
+public class MahasiswaDemo03 {
+    public static void main(String[] args) {
+        StackTugasMahasiswa03 stack = new StackTugasMahasiswa03(5);
+        Scanner alya = new Scanner(System.in);
+        int pilih;
+
+        do {
+            System.out.println("\nMenu : ");
+            System.out.println("1. Mengumpulkan Tugas");
+            System.out.println("2. Menilai Tugas");
+            System.out.println("3. Melihat Tugas Teratas");
+            System.out.println("4. Melihat Daftar Tugas");
+            System.out.print("Pilih : ");
+            pilih = alya.nextInt();
+            alya.nextLine();
+            switch (pilih) {
+                case 1:
+                    System.out.print("Nama    : ");
+                    String nama = alya.nextLine();
+                    System.out.print("NIM     : ");
+                    String nim = alya.nextLine();
+                    System.out.print("Kelas   : ");
+                    String kelas = alya.nextLine();
+                    Mahasiswa03 Mhs = new Mahasiswa03(nama, nim, kelas);
+                    stack.push(Mhs);
+                    System.out.printf("Tugas %s berhasil dikumpulkan\n ", Mhs.nama);
+                    break;
+                case 2:
+                    Mahasiswa03 dinilai = stack.pop();
+                    if (dinilai != null) {
+                        System.out.println("Menilai tugas dari "+ dinilai.nama);
+                        System.out.print("Masukkan nilai (0-100): ");
+                        int nilai = alya.nextInt();
+                        dinilai.tugasDinilai(nilai);
+                        System.out.printf("Nilai Tugas %s adalah %d\n", dinilai.nama, nilai);
+                    }
+                    break;
+                case 3:
+                    Mahasiswa03 lihat = stack.peek();
+                    if (lihat != null) {
+                        System.out.println("Tugas terakhir dikumpulkan oleh "+lihat.nama);
+                    }
+                    break;
+                case 4: 
+                    System.out.println("Daftar semua tugas ");
+                    System.out.println("Nama\tNIM\tKelas ");
+                    stack.print ();
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. ");
+            }
+        } while (pilih >= 1 && pilih <= 4);
+    }
+
+}
---
+++ src/P9/REPORT.md
@@ -0,0 +1,98 @@
+|  | Algoritma dan Struktur Data |
+|--|--|
+| NIM |  254107020152|
+| Nama |  Alya Mukhbita Larassati |
+| Kelas | TI - 1F |
+| Repository | [link] https://github.com/alyamukhbita237-cloud/Praktikum-ASD-2026.git
+
+# JOBSHEET IX STACK
+
+## 2.1 Percobaan 1: Mahasiswa Mengumpulkan Tugas
+
+Hasil Run :
+``` java
+Menu : 
+1. Mengumpulkan Tugas
+2. Menilai Tugas
+3. Melihat Tugas Teratas
+4. Melihat Daftar Tugas
+Pilih : 1
+Nama    : Dila
+NIM     : 1001
+Kelas   : 1A
+Tugas Dila berhasil dikumpulkan
+
+Menu :
+1. Mengumpulkan Tugas
+2. Menilai Tugas
+3. Melihat Tugas Teratas
+4. Melihat Daftar Tugas
+Pilih : 1
+Nama    : Erik
+NIM     : 1002
+Kelas   : 1B
+Tugas Erik berhasil dikumpulkan
+
+Menu :
+1. Mengumpulkan Tugas
+2. Menilai Tugas
+3. Melihat Tugas Teratas
+4. Melihat Daftar Tugas
+Pilih : 3
+Tugas terakhir dikumpulkan oleh Erik
+
+Menu :
+1. Mengumpulkan Tugas
+2. Menilai Tugas
+3. Melihat Tugas Teratas
+4. Melihat Daftar Tugas
+Pilih : 1
+Nama    : Tika
+NIM     : 1003
+Kelas   : 1C
+Tugas Tika berhasil dikumpulkan
+
+Menu :
+1. Mengumpulkan Tugas
+2. Menilai Tugas
+3. Melihat Tugas Teratas
+4. Melihat Daftar Tugas
+Pilih : 4
+Daftar semua tugas 
+Nama    NIM     Kelas
+Dila    1001    1A
+Erik    1002    1B
+Tika    1003    1C
+
+
+Menu :
+1. Mengumpulkan Tugas
+2. Menilai Tugas
+3. Melihat Tugas Teratas
+4. Melihat Daftar Tugas
+Pilih : 2
+Menilai tugas dari Tika
+Masukkan nilai (0-100): 87
+Nilai Tugas Tika adalah 87
+
+Menu :
+1. Mengumpulkan Tugas
+2. Menilai Tugas
+3. Melihat Tugas Teratas
+4. Melihat Daftar Tugas
+Pilih : 4
+Daftar semua tugas
+Nama    NIM     Kelas
+Dila    1001    1A
+Erik    1002    1B
+
+
+Menu :
+1. Mengumpulkan Tugas
+2. Menilai Tugas
+3. Melihat Tugas Teratas
+4. Melihat Daftar Tugas
+Pilih : 5
+Pilihan tidak valid.
+```
+### 2.1.3 Pertanyaan
