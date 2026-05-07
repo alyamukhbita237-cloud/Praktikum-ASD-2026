709072793039789524b77b3c8da24c05931c3b4d
alyamukhbita237 <alyamukhbita237@gmail.com>
2026-05-07 09:45:12 +0700

    P10_Praktikum 1

---
+++ src/P10/Queue03.java
@@ -0,0 +1,149 @@
+package P10;
+
+import java.util.Scanner;
+
+public class Queue03 {
+    int[] data;
+    int front;
+    int rear;
+    int size;
+    int max;
+
+    public Queue03(int n) {
+        max = n;
+        data = new int[max];
+        size = 0;
+        front = rear = -1;
+    }
+
+    public boolean IsEmpty() {
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean IsFull() {
+        if (size == max) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void peek() {
+        if (!IsEmpty()) {
+            System.out.println("Elemen terdepan: " + data[front]);
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    public void print() {
+        if (IsEmpty()) {
+            System.out.println("Queue masih kosong");
+        } else {
+            int i = front;
+            while (i != rear) {
+                System.out.print(data[i] + " ");
+                i = (i + 1) % max;
+            }
+            System.out.println(data[i] + " ");
+            System.out.println("Jumlah elemen = " + size);
+        }
+    }
+
+    public void clear() {
+        if (!IsEmpty()) {
+            front = rear = -1;
+            size = 0;
+            System.out.println("Queue berhasil dikosongkan");
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    public void Enqueue(int dt) {
+        if (IsFull()) {
+            System.out.println("Queue sudah penuh");
+        } else {
+            if (IsEmpty()) {
+                front = rear = 0;
+            } else {
+                if (rear == max - 1) {
+                    rear = 0;
+                } else {
+                    rear++;
+                }
+            }
+            data[rear] = dt;
+            size++;
+        }
+    }
+
+    public int Dequeue() {
+        int dt = 0;
+        if (IsEmpty()) {
+            System.out.println("Queue masih kosong");
+        } else {
+            dt = data[front];
+            size--;
+            if (IsEmpty()) {
+                front = rear = -1;
+            } else {
+                if (front == max - 1) {
+                    front = 0;
+                } else {
+                    front++;
+                }
+            }
+        }
+        return dt;
+    }
+
+    public static void menu() {
+        System.out.println("Masukkan operasi yang diinginkan: ");
+        System.out.println("1. Enqueue ");
+        System.out.println("2. Dequeue");
+        System.out.println("3. Print");
+        System.out.println("4. Peek");
+        System.out.println("5. Clear");
+        System.out.println("--------------------");
+    }
+
+    public static void main(String[] args) {
+        Scanner alya = new Scanner(System.in);
+        System.out.print("Masukkan kapasitas queue: ");
+        int n = alya.nextInt();
+        Queue03 Q = new Queue03(n);
+        int pilih;
+
+        do {
+            menu();
+            pilih = alya.nextInt();
+            switch (pilih) {
+                case 1:
+                    System.out.print("Masukkan data baru: ");
+                    int dataMasuk = alya.nextInt();
+                    Q.Enqueue(dataMasuk);
+                    break;
+                case 2:
+                    int dataKeluar = Q.Dequeue();
+                    if (dataKeluar != 0) {
+                        System.out.println("Data yang dikeluarkan: " + dataKeluar);
+                        break;
+                    }
+                case 3:
+                    Q.print();
+                    break;
+                case 4:
+                    Q.peek();
+                    break;
+                case 5:
+                    Q.clear();
+                    break;
+            }
+        } while (pilih == 1 || pilih == 2 || pilih == 3 || pilih == 4 || pilih == 5);
+    }
+}
---
+++ src/P10/REPORT.md
@@ -0,0 +1,90 @@
+|  | Algoritma dan Struktur Data |
+|--|--|
+| NIM |  254107020152|
+| Nama |  Alya Mukhbita Larassati |
+| Kelas | TI - 1F |
+| Repository | [link] https://github.com/alyamukhbita237-cloud/Praktikum-ASD-2026.git
+
+# JOBSHEET X QUEUE
+
+## 2.1 Percobaan 1 : Operasi Dasar Queue
+
+Hasil Run :
+```java
+Masukkan kapasitas queue: 4
+Masukkan operasi yang diinginkan: 
+1. Enqueue 
+2. Dequeue
+3. Print
+4. Peek
+5. Clear
+--------------------
+1
+Masukkan data baru: 15
+Masukkan operasi yang diinginkan: 
+1. Enqueue 
+2. Dequeue
+3. Print
+4. Peek
+5. Clear
+--------------------
+1
+Masukkan data baru: 31
+Masukkan operasi yang diinginkan: 
+1. Enqueue 
+2. Dequeue
+3. Print
+4. Peek
+5. Clear
+--------------------
+4
+Elemen terdepan: 15
+Masukkan operasi yang diinginkan: 
+1. Enqueue 
+2. Dequeue
+3. Print
+4. Peek
+5. Clear
+--------------------
+6
+```
+
+### 2.1.3. Pertanyaan
+1. Pada konstruktor, mengapa nilai awal atribut front dan rear bernilai -1, sementara atribut size
+bernilai 0?
+- karena front dan rear digunakan sebagai penanda bahwa queue masih kosong dan belum menunjuk ke index manapun sehingga diisi -1. Sedangkan size diisi 0 karena menunjukkan jumlah elemen pada queue
+
+2. Pada method Enqueue, jelaskan maksud dan kegunaan dari potongan kode berikut!
+```java
+if (rear == max-1){
+    rear = 0;
+```
+- kode tersebut untuk mengecek apakah posisi rear sudah berada di indeks terakhir array (max-1), jika sudah di ujung array, maka posisi rear dikembalikan ke indeks 0.
+
+3. Pada method Dequeue, jelaskan maksud dan kegunaan dari potongan kode berikut!
+```java
+if (front == max -1){
+    front = 0;
+```
+- kode tersebut untuk mengecek apakah posisi front sudah berada di indeks terakhir array (max-1). jika iya, maka posisi front dikembalikan ke indeks 0.
+
+4. Pada method print, mengapa pada proses perulangan variabel i tidak dimulai dari 0 (int i=0),
+melainkan int i=front?
+- karena pada queue/circular queue, data pertama tidak selalu berada di indeks 0
+
+5. Perhatikan kembali method print, jelaskan maksud dari potongan kode berikut!
+```java
+i = (i + 1) %max;
+```
+- potongan kode tersebut pada circular queue digunakan untuk memindakan indeks ke posisi berikutnya secara melingkar.
+
+6. Tunjukkan potongan kode program yang merupakan queue overflow!
+```java
+if (IsFull()) {
+    System.out.println("Queue sudah penuh");
+    }
+```
+
+7. Pada saat terjadi queue overflow dan queue underflow, program tersebut tetap dapat berjalan
+dan hanya menampilkan teks informasi. Lakukan modifikasi program sehingga pada saat terjadi
+queue overflow dan queue underflow, program dihentikan!
